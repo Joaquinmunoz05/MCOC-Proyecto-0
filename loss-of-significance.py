4d9c5e9e12119072c9c4a3f826a737b99b1b7ccd
Joaquinmunoz05 <41970395+Joaquinmunoz05@users.noreply.github.com>
2018-08-05 23:38:52 -0400

    Update loss-of-significance.py

--- loss-of-significance.py
+++ loss-of-significance.py
@@ -11,8 +11,8 @@ Y=[y]
 Z=[z]
 for i in range (200):
     b=(Y[-1])
-    b1 = b**3 + b**2 + b      #Ecuaciones cubicas
-    Y.append(np.float16(b1))  
+    b1 = b**3 + b**2 + b
+    Y.append(np.float16(b1))
     c=(Z[-1])
     c1 = c**3 + c**2 + c
     
@@ -22,10 +22,14 @@ print Y[-1]
 print Z[-1]  
 error=[]
 for i in range(len(Y)):
-    error.append((Z[i]-Y[i]))    #diferencia entre cada valor
+    error.append((Z[i]-Y[i]))
 pyplot.plot(error)
+
 pyplot.ylabel("Variacion")
 pyplot.show()
 pyplot.plot(Y,'b')
 pyplot.plot(Z,'r')
+pyplot.ylabel("Valor iteracion")
 pyplot.show()
+for i in range (8):
+    print "valor float16:",Y[i*25], "Valor float64:", Z[i*25], "Diferencia:", error[i*25]
